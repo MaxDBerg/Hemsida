0421957e88f49de4aa7085f814bf677ade938307
MaxDBerg <88880850+MaxDBerg@users.noreply.github.com>
2023-03-27 11:23:00 +0200

    Delete Labb1CvWebbpage/JS directory

--- Labb1CvWebbpage/JS/ModalHeightController.js
+++
@@ -1,8 +0,0 @@
-function resizeModal() {
-    const windowHeight = window.innerHeight;
-    const headerHeight = document.querySelector('header').offsetHeight;
-    const footerHeight = document.querySelector('footer').offsetHeight;
-    const modalPadding = 40;
-    const modalContentHeight = windowHeight - headerHeight - footerHeight - modalPadding;
-    modalContent.style.height = modalContentHeight + 'px';
-  }
